bc894c376189cea6e9567c21087689f37357af2a
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-10 14:27:58 +0300

    commit tree ca136deaab4049481db071e3efec3a4e944ed1ad

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const divide= (parameter1,parameter2) => parameter1/parameter2;
+{
+
+    console.log(`Dividing${parameter1}`);
+    console.log(`Dividing${parameter2}`);
+
+     divide(2000,100);
+
+}
+const square= (x) => x^2; {
+console.log(`squaring ${x}`);
+
+}
+
+const add=(w,y) => {
+console.log(`add${w}`);  
+console.log(`add${y}`);
+return w+y;
+
+
+}
+    
+
