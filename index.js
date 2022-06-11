f18d3469ad911f17b04ffa45b23cf2be634fe038
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-11 12:09:22 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const divide=(parameter1,parameter2) => 
+{ 
+
+2000/100;
+
+return(2000/100);
+}
+
+
+
+const square= (x) => { x**2;
+
+return( x**2);
+
+}
+
+
+const add=(w,y) => {
+
+return w+y;
+
+}
+    
+
